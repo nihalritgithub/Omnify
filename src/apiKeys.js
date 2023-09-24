a66a409dd3ea13c9932b786169ce171fd522d69a
Nihal <nsk91isc@gmail.com>
2023-09-24 21:20:33 +0530

    omnify

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "5072e600b61b78c86eaf148628d1a5ea",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
